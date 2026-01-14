91d0c35c5293ddc13a8cc69dd3710d3f4a40a316
meomeo2102 <giao210204@gmail.com>
2026-01-14 20:59:24 +0700

    Xây dựng model phản hồi đăng nhập

--- lib/models/auth_response.dart
+++ lib/models/auth_response.dart
@@ -1,29 +1,34 @@
 import 'dart:convert';
+import 'customer.dart';
 
-AuthResponse authResponseFromJson(String str) => AuthResponse.fromJson(json.decode(str));
+AuthResponse authResponseFromJson(String str) =>
+    AuthResponse.fromJson(json.decode(str));
 
-String authResponseToJson(AuthResponse data) => json.encode(data.toJson());
+String authResponseToJson(AuthResponse data) =>
+    json.encode(data.toJson());
 
 class AuthResponse {
-  String status;
-  String? token;
-  Map<String, dynamic>? user;
+  final String status;
+  final String? token;
+  final Customer? customer;
 
   AuthResponse({
     required this.status,
     required this.token,
-    required this.user,
+    required this.customer,
   });
 
   factory AuthResponse.fromJson(Map<String, dynamic> json) => AuthResponse(
     status: json["status"],
     token: json["token"],
-    user: json["user"],
+    customer: json["user"] != null
+        ? Customer.fromJson(json["user"])
+        : null,
   );
 
   Map<String, dynamic> toJson() => {
     "status": status,
     "token": token,
-    "user": user,
+    "user": customer?.toJson(),
   };
 }
